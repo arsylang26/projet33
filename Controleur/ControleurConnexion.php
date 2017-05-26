acaffc460b091a7e23e0918bda54d28ddfe1f17d
arsylang26 <fabien.mallier@gmail.com>
2017-05-26 22:05:11 +0200

    26/05 correction de la connexion

--- Controleur/ControleurConnexion.php
+++ Controleur/ControleurConnexion.php
@@ -20,23 +20,24 @@ class ControleurConnexion extends Controleur
     }
     public function connecter()
     {
-        if ($this->requete->existeParametre("admin") && $this->requete->existeParametre("pwd"))
-        {
+        if ($this->requete->existeParametre("admin") && $this->requete->existeParametre("pwd")) {
             $admin = $this->requete->getParametre("admin");
             $pwd = sha1($this->requete->getParametre("pwd")); //cryptage du mot de passe avant de faire la requête sur la BdD
-            if ($this->utilisateur->connecter($admin, $pwd))
-            {
+            if ($this->utilisateur->connecter($admin, $pwd)) {
                 $utilisateur = $this->utilisateur->getUtilisateur($admin, $pwd);
                 $this->requete->getSession()->setAttribut("idAdmin", $utilisateur['idAdmin']);
                 $this->requete->getSession()->setAttribut("admin", $utilisateur['identifiant']);
                 $this->rediriger("accueil");
-            } 
-            else $this->genererVue(array('msgErreur' => 'Identifiant ou mot de passe incorrects', "index"));
-            
-        }
-        else throw new Exception ("Identifiant ou mot de passe incorrects");
-    }
+            } else {
+                $this->getFlash()->error('identifiant ou mot de passe incorrects',null,true);
+                $this->rediriger("connexion");
+            }
 
+        } else {
+            $this->getFlash()->error('il manque soit l\'identifiant soit le mot de passe',null,true);
+            $this->rediriger("connexion");
+        }
+    }
 
     public function deconnecter()
     {
